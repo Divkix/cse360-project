16e7a261e999c59a5e38cc7e5f43b26c8359202e
Divanshu Chauhan <divkix@divkix.me>
2024-04-04 10:42:12 -0700

    we can generate a working jar file now Co-authored-by: 155927887+Chaos012x@users.noreply.github.com

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module me.divkix.cse360project {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
+    requires transitive java.sql;
     requires mysql.connector.j;
 
-
     opens me.divkix.cse360project to javafx.fxml;
+
     exports me.divkix.cse360project;
 }
