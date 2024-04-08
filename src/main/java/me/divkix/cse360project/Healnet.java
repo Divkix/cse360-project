6bd61636b217e824f0b11263fcd1d974ccf10dcf
Divanshu Chauhan <divkix@divkix.me>
2024-04-07 18:46:46 -0700

    code: move initializeDatabase to helpers package
    Co-authored-by: Aryan Namboodiri <Aryankn29@users.noreply.github.com>

--- src/main/java/me/divkix/cse360project/Healnet.java
+++ src/main/java/me/divkix/cse360project/Healnet.java
@@ -9,11 +9,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import me.divkix.cse360project.helperFunctions.sqlHelpers;
+import me.divkix.cse360project.helperFunctions.initDb;
 import me.divkix.cse360project.screens.employeeLogin;
 import me.divkix.cse360project.screens.patient.patientLogin;
-
-import java.util.HashMap;
 
 // Main class
 public class Healnet extends Application {
@@ -28,112 +26,10 @@ public class Healnet extends Application {
     // Main method to launch the application
     public static void main(String[] args) {
         // Initialize the database
-        initializeDatabase();
+        initDb.initializeDatabase();
 
         // Launch the application
         launch(args);
-    }
-
-    // Method to initialize the database
-    // This method will create a new database and a new table for user details
-    // It will also add a new patient, doctor, and nurse to the user details table
-    private static void initializeDatabase() {
-        // Create a new database
-        sqlHelpers.createNewDatabase();
-        // Create a new table for user details
-        sqlHelpers.createNewTable(userDetailsTable, new HashMap<>() {{
-            put("username", "text PRIMARY KEY NOT NULL");
-            put("first_name", "text DEFAULT NULL");
-            put("last_name", "text DEFAULT NULL");
-            put("email", "text DEFAULT NULL");
-            put("phone", "varchar DEFAULT NULL");
-            put("dob", "text DEFAULT NULL");
-            put("address", "text DEFAULT NULL");
-            put("insurance_provider", "text DEFAULT NULL");
-            put("insurance_id", "text DEFAULT NULL");
-            put("role", "text DEFAULT 'patient' NOT NULL");
-            put("password", "text NOT NULL");
-        }});
-        // create a new table for patient details
-        sqlHelpers.createNewTable(patientDetailsTable, new HashMap<>() {{
-            put("username", "text PRIMARY KEY NOT NULL");
-            put("height", "text DEFAULT NULL");
-            put("weight", "text DEFAULT NULL");
-            put("blood_type", "text DEFAULT NULL");
-            put("allergies", "text DEFAULT NULL");
-            put("medications", "text DEFAULT NULL");
-            put("medical_conditions", "text DEFAULT NULL");
-            put("gender", "text DEFAULT NULL");
-        }});
-        // create a table for patient visits
-        sqlHelpers.createNewTable(patientVisitsTable, new HashMap<>() {{
-            put("visit_id", "integer PRIMARY KEY AUTOINCREMENT");
-            put("username", "text NOT NULL");
-            put("doctor_username", "text NOT NULL");
-            put("visit_date", "text NOT NULL");
-            put("visit_reason", "text DEFAULT NULL");
-            put("diagnosis", "text DEFAULT 'TO BE DONE' NOT NULL");
-            put("prescription", "text DEFAULT 'PENDING' NOT NULL");
-        }});
-
-        // Add new patient
-        sqlHelpers.insertDataIntoTable(userDetailsTable, new HashMap<>() {{
-            put("username", "doe_98765");
-            put("role", "patient");
-            put("first_name", "John");
-            put("last_name", "Doe");
-            put("email", "john.doe@example.com");
-            put("phone", "123-456-7890");
-            put("dob", "01/01/1990");
-            put("address", "123 Main St, Anytown, USA");
-            put("insurance_provider", "Blue Cross Blue Shield");
-            put("insurance_id", "123456789");
-            put("password", "password123");
-        }});
-        // Add some health records for the patient
-        sqlHelpers.insertDataIntoTable(patientDetailsTable, new HashMap<>() {{
-            put("username", "doe_98765");
-            put("height", "6'0\"");
-            put("weight", "180 lbs");
-            put("blood_type", "O+");
-            put("allergies", "Peanuts");
-            put("medications", "Aspirin");
-            put("medical_conditions", "Asthma");
-            put("gender", "Male");
-        }});
-        // Add new visit for the patient
-        sqlHelpers.insertDataIntoTable(patientVisitsTable, new HashMap<>() {{
-            put("username", "doe_98765");
-            put("doctor_username", "jane_12345");
-            put("visit_date", "01/01/2021");
-            put("visit_reason", "Annual checkup");
-            put("diagnosis", "Healthy");
-            put("prescription", "None");
-        }});
-        // Add upcoming visit for the patient
-        sqlHelpers.insertDataIntoTable(patientVisitsTable, new HashMap<>() {{
-            put("username", "doe_98765");
-            put("doctor_username", "jane_12345");
-            put("visit_date", "05/05/2024");
-            put("visit_reason", "Stomach Pain");
-        }});
-
-        // Add new doctor
-        sqlHelpers.insertDataIntoTable(userDetailsTable, new HashMap<>() {{
-            put("username", "jane_12345");
-            put("first_name", "Jane");
-            put("last_name", "Smith");
-            put("role", "doctor");
-            put("password", "password123");
-        }});
-        // Add a new nurse
-        sqlHelpers.insertDataIntoTable(userDetailsTable, new HashMap<>() {{
-            put("username", "janice_12345");
-            put("first_name", "Janice");
-            put("last_name", "Johnson");
-            put("role", "nurse");
-            put("password", "password123");
-        }});
     }
 
     // Start method to build the initial view
